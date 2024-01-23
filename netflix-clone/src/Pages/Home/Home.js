2e7f35ebcfd1cf86c3f98ab965d111548ebddcc4
Hailu1248 <88321816+Hailu1248@users.noreply.github.com>
2024-01-23 05:01:02 -0500

    header and footer

---
+++ netflix-clone/src/Pages/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from '../../Componenet/Header/Header'
+import Footer from '../../Componenet/Footer/Footer'
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  )
+}
+
+export default Home
+
