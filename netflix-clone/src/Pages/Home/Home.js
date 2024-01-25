a0f51336d5d3c74c8aef053ad7410207dbbf9c21
Hailu1248 <88321816+Hailu1248@users.noreply.github.com>
2024-01-25 15:03:12 -0500

    banner

--- netflix-clone/src/Pages/Home/Home.js
+++ netflix-clone/src/Pages/Home/Home.js
@@ -1,11 +1,25 @@
 import React from 'react'
 import Header from '../../Componenet/Header/Header'
 import Footer from '../../Componenet/Footer/Footer'
+import Banner from '../../Componenet/Banner/Banner'
 
 const Home = () => {
   return (
     <>
       <Header />
+      <br />
+      <br />
+      <br />
+      <br />
+      <Banner />
+      <br />
+      <br />
+      <br />
+      <br />  
+      <br />
+      <br />
+      <br />
+      <br />
       <Footer />
     </>
   )
